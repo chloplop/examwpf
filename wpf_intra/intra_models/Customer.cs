f62f965f6443b65ebb4f7ec258c2765f72c264b2
chloplop <ch.boudreau@live.ca>
2020-02-24 13:11:43 -0500

    comit 4

---
+++ wpf_intra/intra_models/Customer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace intra_models
+{
+    public class Customer : INotifyPropertyChanged
+    {
+        private string name;
+        private string lastname;
+        private string address;
+        private string city;
+        private string province;
+        private string postalcode;
+        private string contactinfo;
+        private string picturepath;
+        private string info;
+
+        public string Name { get => name; set { name = value; OnPropertyChanged(); } }
+        public string LastName { get => lastname; set { lastname = value; OnPropertyChanged(); } }
+        public string Address { get => address; set { address = value; OnPropertyChanged(); } }
+        public string City { get => city; set { city = value; OnPropertyChanged(); } }
+        public string Province { get => province; set { province = value; OnPropertyChanged(); } }
+        public string PostalCode { get => postalcode; set { postalcode = value; OnPropertyChanged(); } }
+        public string ContactInfo { get => contactinfo; set { contactinfo = value; OnPropertyChanged(); } }
+        public string PicturePath { get => picturepath; set { picturepath = value; OnPropertyChanged(); } }
+        public string Info { get => info; set { info = value; OnPropertyChanged(); } }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
